4b7ffb3b2fe09d80b44b216d88f6ba419faf8174
loveleenahuja <73497147+loveleenahuja@users.noreply.github.com>
2020-10-28 11:30:07 +0100

    Add files via upload

---
+++ src/main/java/com/atm/service/BankService.java
@@ -0,0 +1,50 @@
+package com.atm.service;
+
+import com.atm.model.Account;
+import com.atm.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class BankService
+{
+    @Autowired
+    private AccountRepository accountRepo;
+
+    public Boolean depositCash(Account userAccount, double amount){
+        double new_amount = amount + userAccount.getBalance();
+        userAccount.setBalance(new_amount);
+        return trySaving(userAccount);
+    }
+
+    public Boolean withdrawCash(Account userAccount, double amount){
+        double previous_balance = userAccount.getBalance();
+        if(previous_balance >= amount){
+            double new_amount = userAccount.getBalance() - amount;
+            userAccount.setBalance(new_amount);
+            return trySaving(userAccount);
+        }
+        else{
+            return false;
+        }
+    }
+
+    // Helper
+    public Boolean trySaving(Account userAccount){
+        try {
+            Account updated_account = accountRepo.save(userAccount);
+            if(updated_account != null){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
+}
