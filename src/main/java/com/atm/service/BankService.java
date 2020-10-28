e15f4b97e7271211cb096e60820fc7f7c7364069
loveleenahuja <73497147+loveleenahuja@users.noreply.github.com>
2020-10-28 12:57:17 +0100

    Update BankService.java

--- src/main/java/com/atm/service/BankService.java
+++ src/main/java/com/atm/service/BankService.java
@@ -5,7 +5,7 @@ import com.atm.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
